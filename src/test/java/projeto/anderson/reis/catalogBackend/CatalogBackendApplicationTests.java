de0309eb7c3be2a1ff9d84ae88a83308ec68035d
Anderson reis <anderson.200810@gmail.com>
2023-12-01 14:29:08 -0300

    Inicio do projeto backend

---
+++ src/test/java/projeto/anderson/reis/catalogBackend/CatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.anderson.reis.catalogBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

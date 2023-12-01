643480f0ac2995858b279cf3696bd1352ccc3ef8
Anderson reis <anderson.200810@gmail.com>
2023-12-01 18:59:06 -0300

    Seeding para test

--- src/main/java/projeto/anderson/reis/catalogBackend/services/CategoryService.java
+++ src/main/java/projeto/anderson/reis/catalogBackend/services/CategoryService.java
@@ -2,6 +2,7 @@ package projeto.anderson.reis.catalogBackend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import projeto.anderson.reis.catalogBackend.entities.Category;
 import projeto.anderson.reis.catalogBackend.repository.CategoryRepository;
 
@@ -13,7 +14,7 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }

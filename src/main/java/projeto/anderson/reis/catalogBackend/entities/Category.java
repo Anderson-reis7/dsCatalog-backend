40c9b2ac8d72499ab8963b9d326f881f355b8c1b
Anderson reis <anderson.200810@gmail.com>
2023-12-01 15:08:30 -0300

    Classe Category e o resurso CategoryResource

---
+++ src/main/java/projeto/anderson/reis/catalogBackend/entities/Category.java
@@ -0,0 +1,32 @@
+package projeto.anderson.reis.catalogBackend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
